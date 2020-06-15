3689c751ea130f39101f4c899e33b51ec9abf1a7
Taras Galkovskyi <elimgta@google.com>
2020-06-15 14:47:38 -0400

    restrict download; impose confidence limit

--- detect.py
+++ detect.py
@@ -17,7 +17,7 @@ def detect(source, weights, half, imgsz):
     device = torch_utils.select_device(opt.device)
 
     # Load model
-    google_utils.attempt_download(weights)
+    # google_utils.attempt_download(weights)
     model = torch.load(weights, map_location=device)['model']
     # torch.save(torch.load(weights, map_location=device), weights)  # update model if SourceChangeWarning
     # model.fuse()
@@ -40,36 +40,39 @@ def detect(source, weights, half, imgsz):
     img = torch.zeros((1, 3, imgsz, imgsz), device=device)  # init img
     _ = model(img.half() if half else img.float()) if device.type != 'cpu' else None  # run once
     for path, img, im0s, _ in dataset:
-        img = torch.from_numpy(img).to(device)
-        img = img.half() if half else img.float()  # uint8 to fp16/32
-        img /= 255.0  # 0 - 255 to 0.0 - 1.0
-        if img.ndimension() == 3:
-            img = img.unsqueeze(0)
-
-        # Inference
-        t1 = torch_utils.time_synchronized()
-        pred = model(img, augment=opt.augment)[0]
-
-        # to float
-        if half:
-            pred = pred.float()
-
-        # Apply NMS
-        pred = non_max_suppression(pred, opt.conf_thres, opt.iou_thres,
-                                   fast=True, agnostic=opt.agnostic_nms)
-        t2 = torch_utils.time_synchronized()
-
         predictions[path] = []
-        # Process detections
-        for i, det in enumerate(pred):  # detections per image
-            gn = torch.tensor(im0s.shape)[[1, 0, 1, 0]]  #  normalization gain whwh
-            if det is not None and len(det):
-                # Rescale boxes from img_size to im0 size
-                det[:, :4] = scale_coords(img.shape[2:], det[:, :4], im0s.shape).round()
-                # Save results
-                for *xyxy, conf, cls in det:
-                    box = xyxy
-                    predictions[path].append((conf.tolist(), int(box[0]), int(box[1]), int(box[2]-box[0]), int(box[3]-box[1])))
+        try:
+            img = torch.from_numpy(img).to(device)
+            img = img.half() if half else img.float()  # uint8 to fp16/32
+            img /= 255.0  # 0 - 255 to 0.0 - 1.0
+            if img.ndimension() == 3:
+                img = img.unsqueeze(0)
+
+            # Inference
+            t1 = torch_utils.time_synchronized()
+            pred = model(img, augment=opt.augment)[0]
+
+            # to float
+            if half:
+                pred = pred.float()
+
+            # Apply NMS
+            pred = non_max_suppression(pred, opt.conf_thres, opt.iou_thres,
+                                       fast=True, agnostic=opt.agnostic_nms)
+            t2 = torch_utils.time_synchronized()
+
+            # Process detections
+            for i, det in enumerate(pred):  # detections per image
+                gn = torch.tensor(im0s.shape)[[1, 0, 1, 0]]  #  normalization gain whwh
+                if det is not None and len(det):
+                    # Rescale boxes from img_size to im0 size
+                    det[:, :4] = scale_coords(img.shape[2:], det[:, :4], im0s.shape).round()
+                    # Save results
+                    for *xyxy, conf, cls in det:
+                        box = xyxy
+                        predictions[path].append((conf.tolist(), int(box[0]), int(box[1]), int(box[2]-box[0]), int(box[3]-box[1])))
+        except:
+            pass
 
     print('Done. (%.3fs)' % (time.time() - t0))
     return predictions
@@ -84,23 +87,25 @@ def save_output(source, imgsz, out, predictions):
     # Set Dataloader
     dataset = LoadImages(source, img_size=imgsz)
     results = []
-    for path, img, im0s, _ in dataset:
+    # for path, img, im0s, _ in dataset:
+    for path, preds in predictions.items():
         # Locate tuple 
         pstr = []
 
-        save_path = ''
-        if out:
-            save_path = str(Path(out) / Path(path).name)
-        for pred in predictions[path]:
-            pstr.append('%0.2f %d %d %d %d' % (pred[0], pred[1], pred[2], pred[3], pred[4]))
-            if out:
-                label = '%0.2f' % pred[0]
-                plot_one_box((pred[1], pred[2], pred[1]+pred[3], pred[2]+pred[4]), im0s, label=label, color=(128, 128, 128), line_thickness=3)
+        # save_path = ''
+        # if out:
+        #     save_path = str(Path(out) / Path(path).name)
+        for pred in preds:
+            if pred[0] >= 0.5:
+                pstr.append('%0.2f %d %d %d %d' % (pred[0], pred[1], pred[2], pred[3], pred[4]))
+            # if out:
+            #     label = '%0.2f' % pred[0]
+            #     plot_one_box((pred[1], pred[2], pred[1]+pred[3], pred[2]+pred[4]), im0s, label=label, color=(128, 128, 128), line_thickness=3)
         # # Add bbox to image
         # label = '%s %.2f' % (names[int(cls)], conf)
         # Save results (image with detections)
-        if out:
-            cv2.imwrite(save_path, im0s)
+        # if out:
+        #     cv2.imwrite(save_path, im0s)
         result = {
             'image_id': os.path.splitext(os.path.basename(path))[0],
             'PredictionString': ' '.join(pstr),
@@ -125,9 +130,9 @@ if __name__ == '__main__':
     parser.add_argument('--agnostic-nms', action='store_true', help='class-agnostic NMS')
     parser.add_argument('--augment', action='store_true', help='augmented inference')
     opt = parser.parse_args()
-    print(opt)
+    # print(opt)
 
     with torch.no_grad():
         predictions = detect(opt.source, opt.weights, opt.half, opt.img_size)
-        print(predictions)
+        # print(predictions)
         save_output(opt.source, opt.img_size, opt.output, predictions)
